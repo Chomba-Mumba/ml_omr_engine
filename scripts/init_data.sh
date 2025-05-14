0690bbbcf0054a94376889fe31d802a2816157cd
chomba-mumba <chombaibex@gmail.com>
2025-05-14 22:40:56 +0100

    update

---
+++ scripts/init_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#script to initalise downlaod the data set and copy it to an S3 bucket
+
+echo "initialising dataset in S3..."
+
+# Download dataset
+echo "downloading dataset..."
+
+#Delete the instance
