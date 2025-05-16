31ab39dca9c15e51af6e515189d16762628a4ba4
chomba-mumba <chombaibex@gmail.com>
2025-05-16 11:37:32 +0100

    fixed model encoder and decoder blocks

--- object_detection/models/omr_engine_unet.py
+++ object_detection/models/omr_engine_unet.py
@@ -11,62 +11,85 @@ from tf.keras.models import Sequential
 from tf.keras.layers import Conv2D, Conv2DTranspose, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization, concatenate
 
 class OMREngineUNet(tf.keras.Model):
-    def __init__(self, n_blocks, p_size=None, dropout_prob=0.3, n_layers=2, n_filters=32, kernel_size=3, act_func='relu', kernel_init='HeNormal',pad='same'):
+    def __init__(self, n_blocks, p_size=None, 
+                 dropout_prob=0.3, n_layers=2, n_filters=32, 
+                 kernel_size=3, act_func='relu', kernel_init='HeNormal', 
+                 pad='same', stride=2, n_classes=10):
+        
         super(OMREngineUNet,self).__init__()
+
         #define encoder blocks
         self.skip_connections = []
         self.encoder_blocks = []
         
-        for _ in range(n_blocks):
+        for i in range(1,n_blocks+1):
             temp = []
-            #add conv layers to blocks
-            for j in range(1,n_layers+1):
-                #init conv layer
-                temp.append(Conv2D(16*(j+1),kernel_size, activation=act_func))
 
-            #add batch normalisation to normalise the otput of the last layer
+            #add conv layers to blocks
+            for _ in range(n_layers):
+                temp.append(Conv2D(n_filters*(2**i),
+                                   kernel_size,
+                                   activation=act_func,
+                                   padding=pad,
+                                   kernel_initializer=kernel_init))
+
             temp.append(BatchNormalization())
 
-            #prevent overfiting with dropout
             if dropout_prob > 0:
                 temp.append(Dropout(dropout_prob))
 
             #save skip connection without max pooling to prevent loss
             self.skip_connections.append(keras.Sequential(temp))
 
-            #max pooling for dimensionality reduction
-            if p_size:
+            if p_size and i!=n_blocks:
                 temp.append(MaxPooling2D(pool_size=p_size))
             
-            #add block to encoder block
             self.encoder_blocks.append(keras.Sequential(temp))
-            
+
         #define decoder blocks
         self.decoder_blocks = []
-        for i in range(n_blocks):
+        skip_connection = 0
+
+        for j in range(n_blocks-1,-1,-1):
             temp = []
+
             #merge transposer with corresponding encoder block
-            temp.append(concatenate([Conv2DTranspose(16*(j+1),kernel_size, activation=act_func),self.skip_connections[i]]))
+            temp.append(concatenate([Conv2DTranspose(n_filters*(2**j), (kernel_size, kernel_size), strides=(stride,stride), padding=pad),
+                                     self.skip_connections[skip_connection]],
+                                     axis=3))
 
             #add conv layers to blocks
-            for j in range(1,n_layers+1):
-                #init conv layer
-                temp.append(Conv2D(16*(j+1),kernel_size, activation=act_func))
+            for _ in range(n_layers):
+                temp.append(Conv2D(n_filters*(2**j), 
+                                   kernel_size, 
+                                   activation=act_func,
+                                   padding=pad,
+                                   kernel_initializer=kernel_init))
 
-            #add block to encoder block
-                self.encoder_blocks.append(keras.Sequential(temp))
+            self.decoder_blocks.append(keras.Sequential(temp))
+        
+        #add final layers for the model
+        self.pen_conv = Conv2D(n_filters,
+                    kernel_size,
+                    activation=act_func,
+                    padding='same',
+                    kernel_initializer='he_normal')
 
+        self.final_conv = Conv2D(n_classes, 1, padding='same')
 
 
     def call(self,input):
-        #apply layers to input
-        for layer in self.encoder_blocks:
-            input = layer(input)
+        #apply encoder and decoder blocks
+        for encoder in self.encoder_blocks:
+            input = encoder(input)
+        
+        for decoder in self.decoder_blocks:
+            input = decoder(input)
 
-        #apply pooling
-        input = self.global_pool(input)
+        #apply final layers
+        input = self.pen_conv(input)
 
-        return self.classifier(input)
+        return self.final_conv(input)
 
     def pre_process(self, img):
         # Pull the relevant dimensions for image and mask
@@ -98,5 +121,14 @@ class OMREngineUNet(tf.keras.Model):
             y[index] = single_mask
         return X,y
     
-if __name__ == "__main__":
+if __name__ == "__main__":    # Complete the model with 1 3x3 convolution layer (Same as the prev Conv Layers)
+    # Followed by a 1x1 Conv layer to get the image to the desired size. 
+    # Observe the number of channels will be equal to number of output classes
+    conv9 = Conv2D(n_filters,
+                 3,
+                 activation='relu',
+                 padding='same',
+                 kernel_initializer='he_normal')(ublock9)
+
+    conv10 = Conv2D(n_classes, 1, padding='same')(conv9)
     pass
