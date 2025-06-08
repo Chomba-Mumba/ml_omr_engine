03814afdafd156322b317884242206029e3a70e1
chomba-mumba <chombaibex@gmail.com>
2025-06-08 14:41:29 +0100

    added use of tf.Dataset instead of list

--- object_detection/data_loader.py
+++ object_detection/data_loader.py
@@ -1,68 +1,80 @@
 import tensorflow as tf
+import os
 
 class DataLoader:
-    def __innit__(self, path, batch_size, buffer_size, height=256, width=256):
+    def __init__(self, path, batch_size, buffer_size, height=256, width=256):
         self.path = path
         self.batch_size = batch_size
         self.buffer_size = buffer_size
         self.height = height
-        self.width=256
-    
-    def load_image_train(self, src_path, tar_path):
-        src_image, tar_image = self.load_data(src_path, tar_path)
-        src_image, tar_image = self.random_jitter(src_image,tar_image)
-        src_image, tar_image = self.normalise(src_image, tar_image)
-        return src_image, tar_image
-    
-    def load_iamge_test(self, src_path, tar_path):
-        src_image, tar_image = self.load_data(src_path,tar_path)
-        src_image, tar_image = self.normalise(src_image, tar_image)
-        return src_image, tar_image        
+        self.width=width
 
-    def load_data(self, src_image, tar_image):
-        #read the image as byte string
-        src_image = tf.io.read_file(src_image)
-        src_image  = tf.io.decode_jpeg(src_image) #convert byte string to tensor
+    def load_imgs(self, src_img, tar_img):
 
-        tar_image = tf.io.read_file(tar_image)
-        tar_image  = tf.io.decode_jpeg(tar_image) 
-
-        #convert to float32 tensors
-        src_image = tf.cast(src_image, tf.float32)
-        tar_image = tf.cast(tar_image, tf.float32)
+        src_img,tar_img = tf.io.read_file(src_img), tf.io.read_file(tar_img)# read img as byte string
+        src_img, tar_img  = tf.io.decode_jpeg(src_img), tf.io.decode_jpeg(tar_img) # convert byte string to tensor  
+        src_img, tar_img = tf.cast(src_img, tf.float32), tf.cast(tar_img, tf.float32) # convert to float32 tensor
         
-        return src_image, tar_image
+        return src_img, tar_img
     
-    def resize(src_image, tar_image, height, width):
+    def normalise(self, src, tar):
+        return (src/127.5)-1, (tar/127.5)-1
+    
+    def resize(self, src_image, tar_image, height=286, width=286):
         tf.image.resize(src_image, [height, width],
                         method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
         tf.image.resize(tar_image, [height, width],
                         method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-        
         return src_image, tar_image
-
-    def random_crop(src_image, tar_image):
-        stacked_image = tf.stack([src_image, tar_image], axis=0)
-        cropped_image = tf.image.random_crop(
-            stacked_image, size=[2, IMG_HEIGHT, IMG_WIDTH, 3]
-        )
-        return cropped_image[0], cropped_image[1]
-    
-    def normalise(src_image,tar_image):
-        return (src_image/127.5)-1, (tar_image/127.5)-1
     
     @tf.function()
-    def random_jitter(self,src_image, tar_image):
-        #resize image
-        src_image, tar_image = self.resize(src_image, tar_image, 286, 286)
+    def random_jitter(self, src, tar):
 
-        #crop image
-        src_image, tar_image = self.random_crop(src_image, tar_image)
+        #resize
+        src, tar = self.resize(src, tar)
 
+        #crop images
+        stacked = tf.stack([src, tar],axis=0)
+        cropped = tf.random_crop(stacked,size=[2, self.height, self.width, 3])
+        src, tar = cropped[0], cropped[1]
+
+        #mirror images
         if tf.random.uniform(()) > 0.5:
-            src_image = tf.iamge.flip_left_right(src_image)
-            tar_image = tf.image.flip_left_right(tar_image)
+            src = tf.image.flip_left_right(src)
+            tar = tf.image.flip_left_right(tar)
         
-        return src_image, tar_image
+        return src, tar
 
-    
+    def load_and_preprocess(self, src, tar, augment=True):
+        
+        #load and augment
+        src_img, tar_img = self.load_imgs(src,tar)
+        if augment:
+            src_img, tar_img = self.random_jitter(src_img, tar_img)
+
+        src_img, tar_img = self.normalise(src_img, tar_img)
+
+        return src_img, tar_img
+
+
+        #TODO - split data set, pre-process and test data loader
+    def get_dataset(self, src_path, tar_path, split='train'):
+        src_files = sorted([os.path.join(src_path,f) for f in os.listdir(src_path)])
+        tar_files = sorted([os.path.join(tar_path,f) for f in os.listdir(tar_path)])
+
+        dataset = tf.data.Dataset.from_tensor_slices((src_files,tar_files))
+        
+        if split=="train":
+            dataset = dataset.map(
+                lambda src, tar: self.load_and_preprocess(src, tar, augment=True),
+                num_parallel_calls=tf.data.AUTOTUNE
+            )
+        else:
+            dataset = dataset.map(
+                lambda src, tar: self.load_and_preprocess(src,tar, augment=False),
+                num_parallel_calls=tf.data.AUTOTUNE
+            )
+        return dataset.batch(self.batch_size).prefetch(tf.data.AUTOTUNE)
+
+if __name__ == "__main__":
+    pass
