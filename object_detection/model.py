7f4e23a7c5af4d6e708ad4a6fd819744f06b2e7b
chomba-mumba <chombaibex@gmail.com>
2025-06-08 16:18:32 +0100

    fixed u-net decoder + skip connection concatenation

--- object_detection/model.py
+++ object_detection/model.py
@@ -3,9 +3,9 @@ import numpy as np
 import os
 import keras
 
-from tf.keras import layers
-from tf.keras.models import Sequential
-from tf.keras.layers import Conv2D, Conv2DTranspose, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization, concatenate
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Conv2DTranspose, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization, concatenate
 
 class OMREnginePatchGan(tf.keras.Model):
     def __init__(self, n_blocks, p_size=(2,2), 
@@ -13,7 +13,7 @@ class OMREnginePatchGan(tf.keras.Model):
                  kernel_size=3, act_func='relu', 
                  pad='same', stride=2, n_classes=10):
         
-        super(OMREnginePatchGan,self).__init__()
+        super(OMREnginePatchGan, self).__init__()
 
         kernel_init = tf.random_normal_initializer(0.,0.02)
 
@@ -107,7 +107,7 @@ class OMREngineUNet(tf.keras.Model):
                  kernel_size=3, act_func='relu', kernel_init='HeNormal', 
                  pad='same', stride=2, n_classes=10):
         
-        super(OMREngineUNet,self).__init__()
+        super(OMREngineUNet, self).__init__()
 
         #define encoder blocks
         self.skip_connections = []
@@ -140,17 +140,12 @@ class OMREngineUNet(tf.keras.Model):
 
         #define decoder blocks
         self.decoder_blocks = []
-        skip_connection = 0
 
         for j in range(n_blocks-1,-1,-1):
             temp = []
 
             #merge transposer with corresponding encoder block
-            temp.append(concatenate([Conv2DTranspose(n_filters*(2**j), (kernel_size, kernel_size), strides=(stride,stride), padding=pad),
-                                     self.skip_connections[skip_connection]],
-                                     axis=3))
-
-            skip_connection += 1
+            Conv2DTranspose(n_filters*(2**j), (kernel_size, kernel_size), strides=(stride,stride), padding=pad)
 
             #add conv layers to blocks
             for _ in range(n_layers):
@@ -160,7 +155,7 @@ class OMREngineUNet(tf.keras.Model):
                                    padding=pad,
                                    kernel_initializer=kernel_init))
 
-            self.decoder_blocks.append(keras.Sequential(temp))
+            self.decoder_blocks.append(temp)
         
         #add final layers for the model
         self.pen_conv = Conv2D(n_filters,
@@ -172,7 +167,7 @@ class OMREngineUNet(tf.keras.Model):
         self.final_conv = Conv2D(n_classes, 1, padding='same')
 
         #initilise loss
-        self.loss_obj = tf.keras.losses.BinaryCrossEntropy(from_logits=True)
+        self.loss_obj = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
 
     def call(self,input):
@@ -180,8 +175,12 @@ class OMREngineUNet(tf.keras.Model):
         for encoder in self.encoder_blocks:
             input = encoder(input)
         
-        for decoder in self.decoder_blocks:
-            input = decoder(input)
+        for decoder in range(len(self.decoder_blocks)):
+            input = tf.keras.layers.Concatenate()([
+                self.decoder_blocks[decoder](input),
+                self.skip_connections[decoder]
+                ])
+            
 
         #apply final layers
         input = self.pen_conv(input)
@@ -214,3 +213,7 @@ class OMREngineUNet(tf.keras.Model):
         total_gen_loss = gan_loss + (LAMBDA * l1_loss)
 
         return total_gen_loss, gan_loss, l1_loss
+    
+if __name__ == "__main__":
+    unet = OMREngineUNet(3)
+    print(unet)
