4fbf394619fe9a3e7c42e91fef443f5d7ac4ae24
chomba-mumba <chombaibex@gmail.com>
2025-06-12 10:42:13 +0100

    adjusted image resize and decode so tensor matches batch shape

--- object_detection/data_loader.py
+++ object_detection/data_loader.py
@@ -2,17 +2,21 @@ import tensorflow as tf
 import os
 
 class DataLoader:
-    def __init__(self, batch_size, buffer_size, height=256, width=256):
+    def __init__(self, batch_size, buffer_size, height=1024, width=1024):
         self.batch_size = batch_size
         self.buffer_size = buffer_size
         self.height = height
         self.width=width
 
     def load_imgs(self, src_img, tar_img):
-
+        
         src_img,tar_img = tf.io.read_file(src_img), tf.io.read_file(tar_img)# read img as byte string
-        src_img, tar_img  = tf.io.decode_jpeg(src_img), tf.io.decode_jpeg(tar_img) # convert byte string to tensor  
+        src_img, tar_img  = tf.io.decode_png(src_img , channels=3), tf.io.decode_png(tar_img, channels=3) # convert byte string to tensor 
         src_img, tar_img = tf.cast(src_img, tf.float32), tf.cast(tar_img, tf.float32) # convert to float32 tensor
+
+        #resize
+        src_img, tar_img = self.resize(src_img, tar_img, self.height, self.width)
+        print(src_img.shape)
         
         return src_img, tar_img
     
@@ -20,32 +24,28 @@ class DataLoader:
         return (src/127.5)-1, (tar/127.5)-1
     
     def resize(self, src_image, tar_image, height=286, width=286):
-        tf.image.resize(src_image, [height, width],
+        src_image = tf.image.resize(src_image, [height, width],
                         method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-        tf.image.resize(tar_image, [height, width],
+        tar_image = tf.image.resize(tar_image, [height, width],
                         method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
         return src_image, tar_image
     
     @tf.function()
     def random_jitter(self, src, tar):
 
-        #resize
-        src, tar = self.resize(src, tar)
-
-        #crop images
-        stacked = tf.stack([src, tar],axis=0)
-        cropped = tf.image.random_crop(stacked,size=[2, self.height, self.width, 3])
-        src, tar = cropped[0], cropped[1]
-
-        #mirror images
         if tf.random.uniform(()) > 0.5:
+            #crop images
+            stacked = tf.stack([src, tar],axis=0)
+            cropped = tf.image.random_crop(stacked,size=[2, self.height, self.width, 3])
+            src, tar = cropped[0], cropped[1]
+
+            #mirror images
             src = tf.image.flip_left_right(src)
             tar = tf.image.flip_left_right(tar)
         
         return src, tar
 
     def load_and_preprocess(self, src, tar, augment=True):
-        
         #load and augment
         src_img, tar_img = self.load_imgs(src,tar)
         if augment:
@@ -73,8 +73,12 @@ class DataLoader:
         return dataset.batch(self.batch_size).prefetch(tf.data.AUTOTUNE)
 
 if __name__ == "__main__":
-    loader = DataLoader(32,10)
+    loader = DataLoader(3,10)
+
     src_path = "data/input"
     tar_path = "data/target"
+
     data = loader.get_dataset(src_path, tar_path)
-    print(data)
+
+    # preview tensors
+    print(f"data as iter: {list(data.as_numpy_iterator())}")
