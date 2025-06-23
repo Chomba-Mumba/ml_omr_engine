be89bea59a208124c53bbd7ddb010aa5539fc0e3
Chomba-Mumba <chombaibex@gmail.com>
2025-06-23 11:34:42 +0100

    removed nested for loop

--- scripts/build_and_push.sh
+++ scripts/build_and_push.sh
@@ -8,15 +8,13 @@ for dir in ./*; do
     fi
 
     #check for dockerfile and build
-    for file in ./*; do
-        if [ "${file}" = "Dockerfile" ]; then
-            image_uri="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO_NAME}:${IMAGE_TAG}"
+    if [-f "$dir/Dockerfile" ]; then
+        image_uri="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO_NAME}:${IMAGE_TAG}"
 
-            echo "building docker image ${dir}:${IMAGE_TAG}"
-            docker build -t "$image_uri" "$dir"
+        echo "building docker image ${dir}:${IMAGE_TAG}"
+        docker build -t "$image_uri" "$dir"
 
-            echo "pushing image to docker..."
-            docker push "$image_uri"
-        fi
-    done
+        echo "pushing image to docker..."
+        docker push "$image_uri"
+    fi
 done
